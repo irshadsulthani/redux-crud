08f1ddf96764c48fcec8ed2a680514c13291d9db
irshadsulthani <irshadpallar2020@gmail.com>
2025-03-25 17:11:56 +0530

    changes

--- recux-crud/src/components/AdminNavbar.jsx
+++ recux-crud/src/components/AdminNavbar.jsx
@@ -22,6 +22,12 @@ function AdminNavbar() {
     }
   }
 
+
+
+
+
+  
+
   return (
     <nav className="bg-gray-900 sticky top-0 z-50 text-white py-4 px-6 shadow-md flex items-center">
       {/* Left - Logo */}
