8c648ad006e152984800335b961f498848f71735
irshadsulthani <irshadpallar2020@gmail.com>
2025-03-26 15:18:49 +0530

    changes

--- recux-crud/src/components/Navbar.jsx
+++ recux-crud/src/components/Navbar.jsx
@@ -2,11 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
-
-
-
-
-
 function Navbar() {
   const { currentUser } = useSelector((state) => state.user);
 
