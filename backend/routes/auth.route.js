6d8dbfca9dbb85ec45e19eeb9a2c7aef0db75e6b
irshadsulthani <irshadpallar2020@gmail.com>
2025-04-06 20:00:41 +0530

    changes

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,8 +1,6 @@
 import express from 'express'
 import { google, logout, signIn, signUp } from '../controllers/auth.controller.js';
 
-
-
 const router = express.Router();
 
 router.post('/signUp', signUp)
